f80c95d0904f3ba2b30c542ebf2bdbe7990ef8f7
DeboraMendes <debora.mendes@live.com>
2023-03-13 19:53:34 -0300

    Initial commit

---
+++ src/main/java/com/objectorientedprogramming/pillars/PillarsOopApplication.java
@@ -0,0 +1,13 @@
+package com.objectorientedprogramming.pillars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PillarsOopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PillarsOopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/objectorientedprogramming/pillars/PillarsOopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.objectorientedprogramming.pillars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PillarsOopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
